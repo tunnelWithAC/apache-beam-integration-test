a0f5ea74cc85e6929646135541d5376e69517f92
conall <conalldalydev@gmail.com>
2021-03-28 11:40:55 +0100

    testing scripts2

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -1,1 +1,6 @@
-pytest --log-cli-level=INFO tests/pubsub_it_test.py --test-pipeline-options="--runner=TestDataflowRunner --project=${{ env.PROJECT }} --region=europe-west1 --staging_location=gs://${{ env.BUCKET }}/staging --temp_location=gs://${{ env.BUCKET }}/temp --job_name=it-test-pipeline --setup_file ./setup.py"
+pytest --log-cli-level=INFO tests/pubsub_it_test.py --test-pipeline-options="--runner=TestDataflowRunner \
+            --project=${{ env.PROJECT }} --region=europe-west1 \
+            --staging_location=gs://${{ env.BUCKET }}/staging \
+            --temp_location=gs://${{ env.BUCKET }}/temp \
+            --job_name=it-test-pipeline \
+            --setup_file ./setup.py"
